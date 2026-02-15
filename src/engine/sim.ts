2627169a4f6a02071be4bba6449b5b18e25b80d4
t3dy <t3dy@users.noreply.github.com>
2026-02-14 21:21:23 -0800

    Final fix for v7.0: Repaired sim.ts class structure

--- src/engine/sim.ts
+++ src/engine/sim.ts
@@ -56,29 +56,29 @@ export class Simulation {
             if (this.isWaitingForChoice) break; // Pause simulation
         }
 
-        applyBroodBonds(snakes: SnakeState[]) {
-            snakes.forEach(s1 => {
-                const nearbyAllies = snakes.filter(s2 =>
-                    s1.id !== s2.id &&
-                    s1.team === s2.team &&
-                    Math.abs(s1.pos.x - s2.pos.x) <= 2 &&
-                    Math.abs(s1.pos.y - s2.pos.y) <= 2
-                );
-
-                if (nearbyAllies.length > 0) {
-                    // Broad Bond: Boost Speed/Agility when near allies
-                    s1.currentStats.agility = s1.baseStats.agility + 2;
-                    if (!s1.statuses.includes('Bonded')) s1.statuses.push('Bonded');
-                } else {
-                    s1.currentStats.agility = s1.baseStats.agility;
-                    s1.statuses = s1.statuses.filter(st => st !== 'Bonded');
-                }
-            });
-        }
-
         if (!this.isWaitingForChoice) {
             this.resolveCombat();
         }
+    }
+
+    applyBroodBonds(snakes: SnakeState[]) {
+        snakes.forEach(s1 => {
+            const nearbyAllies = snakes.filter(s2 =>
+                s1.id !== s2.id &&
+                s1.team === s2.team &&
+                Math.abs(s1.pos.x - s2.pos.x) <= 2 &&
+                Math.abs(s1.pos.y - s2.pos.y) <= 2
+            );
+
+            if (nearbyAllies.length > 0) {
+                // Broad Bond: Boost Speed/Agility when near allies
+                s1.currentStats.agility = s1.baseStats.agility + 2;
+                if (!s1.statuses.includes('Bonded')) s1.statuses.push('Bonded');
+            } else {
+                s1.currentStats.agility = s1.baseStats.agility;
+                s1.statuses = s1.statuses.filter(st => st !== 'Bonded');
+            }
+        });
     }
 
     // New API for external choice injection
